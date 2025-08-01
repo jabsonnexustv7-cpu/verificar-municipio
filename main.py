923fbed7688fcabe01d0f3868d8741e29a0a6ed1
jabsonnexustv7-cpu <jabsonnexustv7@gmail.com>
2025-08-01 11:21:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 # Bloco 1: Tio Sam
 tio_sam_cidades = [
-    "Aguaí", "Bady Bassitt", "Cravinhos", "Cubatão", "Franca", "Indaiatuba", "Itapetininga", "Itú",
-    "Itupeva", "Leme", "Limeira", "Lins", "Mirassol", "Mogi Guaçu", "Mogi Mirim", "Peruíbe", "Pirassununga",
-    "Porto Ferreira", "Salto", "São José do Rio Preto", "Artur Nogueira", "Cosmópolis", "Estiva Gerbi",
-    "Tambaú", "Santa Rita do Passa Quatro", "Santa Cruz das Palmeiras", "Casa Branca", "Cedral"
+    "Aguaí", "Bady Bassitt", "Cravinhos", "Cubatão", "Franca", "Indaiatuba", "Itapetininga", "Itu",
+    "Itupeva", "Leme", "Limeira", "Lins", "Mirassol", "Mogi Guaçu", "Mogi Mirim", "Peruíbe",
+    "Pirassununga", "Porto Ferreira", "Salto", "São José do Rio Preto", "Artur Nogueira", "Cosmópolis",
+    "Estiva Gerbi", "Tambaú", "Santa Rita do Passa Quatro", "Santa Cruz das Palmeiras", "Casa Branca", "Cedral"
 ]
 
 # Bloco 2: Barretos
@@ -18,32 +18,29 @@ barretos_cidades = [
 # Bloco 3: Cidades gerais
 cidades_gerais = [
     "Aguaí", "Águas de Santa Bárbara", "Alumínio", "Americana", "Américo Brasiliense", "Amparo",
-    "Araçariguama", "Araraquara", "Araras", "Artur Nogueira", "Atibaia", "Bady Bassitt", "Barretos",
-    "Bebedouro", "Biritiba-Mirim", "Boa Esperança do Sul", "Bom Jesus dos Perdões", "Borborema",
+    "Araçariguama", "Araçoiaba", "Araras", "Artur Nogueira", "Atibaia", "Bady Bassitt", "Barretos",
+    "Bebedouro", "Birigui", "Biritiba-Mirim", "Boa Esperança do Sul", "Bom Jesus dos Perdões", "Borborema",
     "Bragança Paulista", "Cabreúva", "Caçapava", "Caieiras", "Campinas", "Campo Limpo Paulista",
     "Cândido Rodrigues", "Capivari", "Casa Branca", "Cedral", "Colina", "Conchal", "Cordeirópolis",
-    "Cosmópolis", "Cravinhos", "Cristais Paulista", "Cubatão", "Descalvado", "Dobrada", "Engenheiro Coelho",
-    "Estiva Gerbi", "Fernando Prestes", "Franca", "Francisco Morato", "Franco da Rocha", "Gavião Peixoto",
-    "Guaíra", "Guapiçu", "Guararema", "Guariba", "Guarujá", "Guatapará", "Holambra", "Hortolândia",
-    "Ibaté", "Ibitinga", "Igaratá", "Indaiatuba", "Iracemápolis", "Itajobi", "Itaju", "Itanhaém", "Itápolis",
-    "Itirapúã", "Itupeva", "Jaborandi", "Jaboticabal", "Jacareí", "Jaguariúna", "Jarinu", "Jundiaí", "Leme",
-    "Limeira", "Lindóia", "Louveira", "Mairiporã", "Matão", "Mirassol", "Mogi das Cruzes", "Mogi Guaçu",
-    "Mogi Mirim", "Mongaguá", "Monte Alto", "Monte Mor", "Motuca", "Nazaré Paulista", "Nova Europa",
-    "Nova Odessa", "Olímpia", "Patrocínio Paulista", "Paulínia", "Pedreira", "Peruíbe", "Pindorama",
-    "Piracaia", "Piracicaba", "Pirajuí", "Pirassununga", "Pitangueiras", "Porto Ferreira", "Praia Grande",
-    "Rafard", "Ribeirão Bonito", "Ribeirão Corrente", "Ribeirão Preto", "Rincão", "Salesópolis",
-    "Santa Adélia", "Santa Bárbara d'Oeste", "Santa Branca", "Santa Cruz das Palmeiras", "Santa Ernestina",
-    "Santa Gertrudes", "Santa Lúcia", "Santa Rita do Passa Quatro", "Santa Rosa de Viterbo",
-    "Santo Antônio de Posse", "Santos", "São Bernardo do Campo", "São Carlos", "São José do Rio Preto",
-    "São José dos Campos", "São Vicente", "Serra Azul", "Serra Negra", "Sorocaba", "Sumaré", "Tabatinga",
-    "Tambaú", "Taquaritinga", "Taubaté", "Trabiju", "Tremembé", "Uchoa", "Valinhos", "Várzea Paulista",
-    "Vinhedo", "Votorantim"
+    "Cosmópolis", "Cravinhos", "Cubatão", "Descalvado", "Dracena", "Elias Fausto", "Embu-Guaçu",
+    "Estiva Gerbi", "Fernandópolis", "Franca", "Francisco Morato", "Guaíra", "Guariba", "Guarujá",
+    "Guarulhos", "Hortolândia", "Indaiatuba", "Ipeúna", "Itapecerica da Serra", "Itapetininga",
+    "Itapevi", "Itatiba", "Itu", "Itupeva", "Jaguariúna", "Jales", "Jandira", "Jardinópolis",
+    "Jarinu", "Jaú", "Joanópolis", "Jundiaí", "Leme", "Limeira", "Lins", "Mauá", "Miguelópolis",
+    "Mirassol", "Mogi Guaçu", "Mogi Mirim", "Monte Alto", "Monte Azul Paulista", "Monte Mor",
+    "Nova Odessa", "Olímpia", "Orlândia", "Osasco", "Paulínia", "Penápolis", "Peruíbe",
+    "Pindamonhangaba", "Piracicaba", "Pirassununga", "Pitangueiras", "Pontal", "Porto Ferreira",
+    "Praia Grande", "Ribeirão Preto", "Rio Claro", "Rio das Pedras", "Salto", "Santa Bárbara d'Oeste",
+    "Santa Cruz das Palmeiras", "Santa Rita do Passa Quatro", "Santo André", "Santos", "São Bernardo do Campo",
+    "São Caetano do Sul", "São Carlos", "São José do Rio Preto", "São José dos Campos", "São Paulo",
+    "Sertãozinho", "Sorocaba", "Sumaré", "Suzano", "Tabapuã", "Tabatinga", "Tambaú", "Taquaritinga",
+    "Taubaté", "Tatuí", "Valinhos", "Vargem Grande do Sul", "Várzea Paulista", "Vinhedo", "Votorantim"
 ]
 
 @app.route('/verificar-cidade')
 def verificar_cidade():
     nome_cidade = request.args.get('nome')
-    
+
     if not nome_cidade:
         return jsonify({"erro": "Parâmetro 'nome' é obrigatório"}), 400
 
@@ -59,3 +56,6 @@ def verificar_cidade():
         return jsonify({"atendida": True, "cidade": cidade_formatada, "grupo": "Padrão"})
 
     return jsonify({"atendida": False, "cidade": cidade_formatada, "grupo": None})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
